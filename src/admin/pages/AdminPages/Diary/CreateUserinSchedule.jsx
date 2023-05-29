9c0322053a912bce9b9c8a6a8faaecfae0be4ebb
Lara Alonso <almudena85perez@gmail.com>
2023-05-29 19:05:37 +0200

    GetonDiary

--- src/admin/pages/AdminPages/Diary/CreateUserinSchedule.jsx
+++ src/admin/pages/AdminPages/Diary/CreateUserinSchedule.jsx
@@ -3,13 +3,17 @@ import { useState } from 'react';
 export const CreateUserinSchedule = () => {
   const [idInput, setIdInput] = useState('');
   const [usuarioPutInput, setUsuarioPutInput] = useState('');
+  const [users, setUsers] = useState([])
+  const [menuOpened, setMenuOpened] = useState(false)
+  const [menuOpened2, setMenuOpened2] = useState(false)
+  const [schedule, setSchedule] = useState([])
 
 
-  const url =  'http://localhost:8080/'
+  const url = 'http://localhost:8080/'
 
   //Función actualizar usuario
   async function onSubmitUpdateUseronSchedule(event) {
-    
+
     event.preventDefault();
     try {
 
@@ -34,11 +38,11 @@ export const CreateUserinSchedule = () => {
       const data = await response.json();
 
       if (response.status !== 200) {
-        if(response.status === 400){
+        if (response.status === 400) {
           alert("El usuario ya está registrado en este horario")
           return;
-        }else
-        throw data.error || new Error(`Request failed with status ${response.message}`);
+        } else
+          throw data.error || new Error(`Request failed with status ${response.message}`);
       }
       alert(`Usuario ${usuarioPutInput} añadido`)
 
@@ -49,6 +53,61 @@ export const CreateUserinSchedule = () => {
     }
   }
 
+  //Función obtener usuarios.
+  async function onSubmitGetUsers(event) {
+
+    event.preventDefault();
+    try {
+
+      const response = await fetch(`${url}api/usuarios/auth/?limite=100`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw data.error || new Error(`${response.message}`);
+      }
+      setUsers(data.usuarios);
+
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert("No se han podido obtener los usuarios.");
+    }
+  }
+
+  //Función obtener horarios.
+  async function onSubmitGetSchedule(event) {
+
+    event.preventDefault();
+    try {
+
+      const response = await fetch(`${url}api/horarios/auth/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw data.error || new Error(`${response.message}`);
+      }
+      setSchedule(data.horarios);
+
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert("No se han podido mostrar los horarios");
+    }
+  }
+
+
 
 
   return (
@@ -56,12 +115,12 @@ export const CreateUserinSchedule = () => {
       <br />
       <form className='formAdmin' onSubmit={onSubmitUpdateUseronSchedule} >
         <h1>Añadir usuario</h1>
-        <br/>
+        <br />
         <label>Id horario </label>
         <input
           type="text"
           name="id"
-          placeholder="Id"
+          placeholder="645f3ebfaafb066077be9a42"
           value={idInput}
           onChange={(e) => setIdInput(e.target.value)}
         />
@@ -73,11 +132,75 @@ export const CreateUserinSchedule = () => {
           placeholder="Id usuario"
           value={usuarioPutInput}
           onChange={(e) => setUsuarioPutInput(e.target.value)}
-        />      
+        />
         <input className='botonForm' type="submit" value="Añadir usuario" />
       </form>
+      <br />
+      {/* Mostrar usuarios */}
+      <form onSubmit={onSubmitGetUsers} >
+        <input onClick={() => setMenuOpened(!menuOpened)} className='boton' type="submit" value="Mostrar usuarios" />
 
 
+      </form>
+      <div className={menuOpened ? 'nav-menu_visible' : 'nav-menu_visible hidden'} >
+        <ol className='botonol'>
+          {
+            users.map(({ uid, nombre, correo, dni, telefono, actividad, horario }) => (
+              <li key={uid} className='boton'>
+                id={uid}
+                <br />
+                {nombre}
+                <br />
+                {telefono}
+                <br />
+                {correo}
+                <br />
+                {dni}
+                <br />
+                {actividad}
+                <br />
+                {horario}
+                <br />
+              </li>
+            ))
+
+          }
+        </ol>
+      </div>
+      {/* Mostrar horarios */}
+      <br />
+      <form onSubmit={onSubmitGetSchedule} >
+        <input onClick={() => setMenuOpened2(!menuOpened2)} className="boton" type="submit" value="Mostrar horarios" />
+
+      </form>
+      <div className={menuOpened2 ? 'nav-menu_visible' : 'nav-menu_visible hidden'}>
+        <ol >
+          {
+            schedule.map(({ _id, dia, desde, hasta, sala, actividad, nombreActividad }) => (
+              <li key={_id} className='boton'>
+                id={_id}
+                <br />
+                {dia}
+                <br />
+                {sala}
+                <br />
+                {desde}
+                <br />
+                {hasta}
+                <br />
+                {nombreActividad} ({actividad})
+                <br />
+
+                <br />
+              </li>
+            ))
+
+          }
+        </ol>
+      </div>
     </>
+
   )
+
 }
+
--- src/admin/pages/AdminPages/Diary/DeleteUsertoSchedule.jsx
+++ src/admin/pages/AdminPages/Diary/DeleteUsertoSchedule.jsx
@@ -3,9 +3,13 @@ import { useState } from 'react';
 export const DeleteUsertoSchedule = () => {
   const [idInput, setIdInput] = useState('');
   const [usuarioInput, setUsuarioInput] = useState('');
+  const [users, setUsers] = useState([])
+  const [menuOpened, setMenuOpened] = useState(false)
+  const [menuOpened2, setMenuOpened2] = useState(false)
+  const [schedule, setSchedule] = useState([])
 
 
-  const url =  'http://localhost:8080/'
+  const url = 'http://localhost:8080/'
   //Función actualizar usuario
   async function onSubmitDeleteUseronSchedule(event) {
 
@@ -34,18 +38,18 @@ export const DeleteUsertoSchedule = () => {
           "Content-Type": "application/json",
         }
 
-      });  
+      });
 
       const data = await response.json();
-  
+
       if (response.status !== 200) {
         if (response.status === 400) {
           alert(`El usuario no existe en este horario.`)
           return;
-        } else {          
+        } else {
           throw data.error || new Error(`Request failed with status ${response.message}`);
         }
-      }      
+      }
       alert(`Usuario ${usuarioInput} borrado`)
 
     } catch (error) {
@@ -54,6 +58,60 @@ export const DeleteUsertoSchedule = () => {
       alert("No se ha podido borrar al usuario del horario.");
     }
   }
+  //Función obtener usuarios.
+  async function onSubmitGetUsers(event) {
+
+    event.preventDefault();
+    try {
+
+      const response = await fetch(`${url}api/usuarios/auth/?limite=100`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw data.error || new Error(`${response.message}`);
+      }
+      setUsers(data.usuarios);
+
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert("No se han podido obtener los usuarios.");
+    }
+  }
+
+  //Función obtener horarios.
+  async function onSubmitGetSchedule(event) {
+
+    event.preventDefault();
+    try {
+
+      const response = await fetch(`${url}api/horarios/auth/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw data.error || new Error(`${response.message}`);
+      }
+      setSchedule(data.horarios);
+
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert("No se han podido mostrar los horarios");
+    }
+  }
+
 
 
 
@@ -82,7 +140,68 @@ export const DeleteUsertoSchedule = () => {
         />
         <input className='botonForm' type="submit" value="Borrar usuario" />
       </form>
+      {/* Mostrar usuarios */}
+      <form onSubmit={onSubmitGetUsers} >
+        <input onClick={() => setMenuOpened(!menuOpened)} className='boton' type="submit" value="Mostrar usuarios" />
 
+
+      </form>
+      <div className={menuOpened ? 'nav-menu_visible' : 'nav-menu_visible hidden'} >
+        <ol className='botonol'>
+          {
+            users.map(({ uid, nombre, correo, dni, telefono, actividad, horario }) => (
+              <li key={uid} className='boton'>
+                id={uid}
+                <br />
+                {nombre}
+                <br />
+                {telefono}
+                <br />
+                {correo}
+                <br />
+                {dni}
+                <br />
+                {actividad}
+                <br />
+                {horario}
+                <br />
+              </li>
+            ))
+
+          }
+        </ol>
+      </div>
+      {/* Mostrar horarios */}
+      <br />
+      <form onSubmit={onSubmitGetSchedule} >
+        <input onClick={() => setMenuOpened2(!menuOpened2)} className="boton" type="submit" value="Mostrar horarios" />
+
+      </form>
+      <div className={menuOpened2 ? 'nav-menu_visible' : 'nav-menu_visible hidden'}>
+        <ol >
+          {
+            schedule.map(({ _id, dia, desde, hasta, sala, actividad, nombreActividad }) => (
+              <li key={_id} className='boton'>
+                id={_id}
+                <br />
+                {dia}
+                <br />
+                {sala}
+                <br />
+                {desde}
+                <br />
+                {hasta}
+                <br />
+                {nombreActividad} ({actividad})
+                <br />
+
+                <br />
+              </li>
+            ))
+
+          }
+        </ol>
+      </div>
 
     </>
   )
